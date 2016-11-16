a2c0ee4cbe10442bf48b974e9c3c9d983656e405
Raghu Betina <raghu@firstdraft.com>
2016-11-16 01:27:02 +0000

    Generate actors resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Actor resource:
+  # CREATE
+  get "/actors/new", :controller => "actors", :action => "new"
+  post "/create_actor", :controller => "actors", :action => "create"
+
+  # READ
+  get "/actors", :controller => "actors", :action => "index"
+  get "/actors/:id", :controller => "actors", :action => "show"
+
+  # UPDATE
+  get "/actors/:id/edit", :controller => "actors", :action => "edit"
+  post "/update_actor/:id", :controller => "actors", :action => "update"
+
+  # DELETE
+  get "/delete_actor/:id", :controller => "actors", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
